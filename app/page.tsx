b9c7ed26ca47edeee1c02e8aec0a4797d812a2ca
minjun-P <jun7332568@gmail.com>
2024-04-28 15:37:52 +0900

    feat :메인 임시 안내 페이지

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+export const revalidate = 100; 
 import { sql } from "@vercel/postgres";
-import Image from "next/image";
 
 export default async function Home() {
   const didDocs = await sql`SELECT * FROM registry`
   return (
-    <div>
-      did web registry를 위한 웹
-      {didDocs.rows.length}
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <h2 className="text-2xl font-bold">
+        did web registry를 위한 임시 next js
+      </h2>
       <div>주로 route handler를 활용한다</div>
+      <p>현재 {didDocs.rowCount}개의 did doc이 등록되어 있습니다</p>
     </div>
   );
 }
